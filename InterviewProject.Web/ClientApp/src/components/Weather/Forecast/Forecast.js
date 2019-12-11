866ed7af761ccd567279bbc78633a8eb4eaf46ad
Jason Brown <jason.brown91@outlook.com>
2019-12-11 15:33:36 -0700

    Finished styling of ForecastItems using actual data from WeatherApi

--- InterviewProject.Web/ClientApp/src/components/Weather/Forecast/Forecast.js
+++ InterviewProject.Web/ClientApp/src/components/Weather/Forecast/Forecast.js
@@ -4,7 +4,7 @@ import ForecastItem from "./ForecastItem/ForecastItem";
 
 const Forecast = (props) => {
     const {forecasts} = props
-    let forecastItems = forecasts.map(day => <ForecastItem key={day.date} weatherInfo={day}/>);
+    let forecastItems = forecasts.map((day, i) => <ForecastItem key={day.id} today={i === 0} weatherInfo={day}/>);
     return (
         <div className={Styles.wrapper}>
             <h2>Salt Lake City, UT</h2>
--- InterviewProject.Web/ClientApp/src/components/Weather/Forecast/ForecastItem/ForecastItem.js
+++ InterviewProject.Web/ClientApp/src/components/Weather/Forecast/ForecastItem/ForecastItem.js
@@ -3,20 +3,28 @@ import Styles from "./ForecastItem.module.scss";
 
 const ForecastItem = (props) => {
 
-    const {weatherInfo} = props;
-    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const {today, weatherInfo: {applicable_date, weather_state_abbr, weather_state_name, 
+                            min_temp, max_temp, wind_speed, wind_direction}} = props;
+    const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const iconBaseUrl = "https://www.metaweather.com/static/img/weather/png/"
+
+    function toFahrenheit (value) {
+        return Math.round(32 + (value / 0.5556))
+    }
 
     return (
         <div className={Styles.wrapper}>
-            <h3>{weekDays[new Date(weatherInfo.date).getDay()]}</h3>
-            <img className={Styles.conditionImage} src="https://cdn0.iconfinder.com/data/icons/ecology-111/1022/clouds-512.png" 
+            <h3>{today ? "Today" : weekDays[new Date(applicable_date).getDay()]}</h3>
+            <img className={Styles.conditionImage} src={`${iconBaseUrl}${weather_state_abbr}.png`} 
                     alt="Weather summary"/>
-            <p>Weather Summary</p>
-            <span>Low: 28</span>
-            <span>High: 40</span>
-            <p>Wind 5mph <img className={Styles.windDirectionImage} 
+            <p>{weather_state_name}</p>
+            <span>Low: {toFahrenheit(min_temp)}°</span>
+            <span>High: {toFahrenheit(max_temp)}°</span>
+            <p>Wind: {`${Math.round(wind_speed)}mph`} <img className={Styles.windDirectionImage} 
                                 src={require("../../../../images/arrow.png")} 
-                                alt="Wind Direction"/></p>
+                                alt="Wind Direction"
+                                style={{transform: `rotate(${Math.round(wind_direction)}deg)`}}/>
+                                </p>
        </div>
     );
 }
